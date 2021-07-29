b7d0b7a159c8c48788330b330db80d6231040c1c
fayax555 <42441836+fayax555@users.noreply.github.com>
2021-07-29 20:18:00 +0500

    update Embed.ts

--- components/Dashboard/SlateEditor/Embeds.ts
+++ components/Dashboard/SlateEditor/Embeds.ts
@@ -2,55 +2,51 @@ import { Editor, Transforms } from 'slate';
 
 export const Embeds = (e: any, editor: Editor) => {
   const pastedText = e.clipboardData?.getData('text')?.trim();
-  console.log(e.clipboardData.getData('text'));
 
   if (pastedText.includes('twitter.com/') && e.key !== 'Enter') {
+    if (!pastedText.match(/status[^]*/)) return;
+
+    const matches = pastedText.match(/status[^]*/)[0].replace(/\D/g, '');
+
     e.preventDefault();
 
-    try {
-      editor.isVoid = (el) => el.type === 'tweet';
-      const matches = pastedText.match(/status[^]*/)![0].replace(/\D/g, '');
-      Transforms.insertNodes(editor, [
-        {
-          type: 'tweet',
-          tweetId: matches,
-          children: [
-            {
-              text: '',
-            },
-          ],
-        },
-        { type: 'paragraph', children: [{ text: '' }] },
-      ]);
-    } catch (error) {
-      console.log(error);
-    }
+    editor.isVoid = (el) => el.type === 'tweet';
+    Transforms.insertNodes(editor, [
+      {
+        type: 'tweet',
+        tweetId: matches,
+        children: [
+          {
+            text: '',
+          },
+        ],
+      },
+      { type: 'paragraph', children: [{ text: '' }] },
+    ]);
   }
 
   if (pastedText.includes('youtube.com/')) {
+    const youtubeRegex =
+      /^(?:(?:https?:)?\/\/)?(?:(?:www|m)\.)?(?:(?:youtube\.com|youtu.be))(?:\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(?:\S+)?$/;
+    const matches = pastedText.match(youtubeRegex);
+
+    if (!matches) return;
+
     e.preventDefault();
 
-    try {
-      editor.isVoid = (el) => el.type === 'youtube';
-      const youtubeRegex =
-        /^(?:(?:https?:)?\/\/)?(?:(?:www|m)\.)?(?:(?:youtube\.com|youtu.be))(?:\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(?:\S+)?$/;
-      const matches = pastedText.match(youtubeRegex);
-      console.log(matches);
-      const [_, videoId] = matches;
-      Transforms.insertNodes(editor, [
-        {
-          type: 'youtube',
-          videoId,
-          children: [
-            {
-              text: '',
-            },
-          ],
-        },
-        { type: 'paragraph', children: [{ text: '' }] },
-      ]);
-    } catch (error) {
-      console.log(error);
-    }
+    editor.isVoid = (el) => el.type === 'youtube';
+    const [_, videoId] = matches;
+    Transforms.insertNodes(editor, [
+      {
+        type: 'youtube',
+        videoId,
+        children: [
+          {
+            text: '',
+          },
+        ],
+      },
+      { type: 'paragraph', children: [{ text: '' }] },
+    ]);
   }
 };
